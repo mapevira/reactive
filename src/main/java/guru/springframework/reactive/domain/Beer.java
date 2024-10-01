450aae0159d3f473bd84ac3e177ca131a17d4a69
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-10-01 15:57:17 +0200

    264. Create Database Entity

---
+++ src/main/java/guru/springframework/reactive/domain/Beer.java
@@ -0,0 +1,72 @@
+package guru.springframework.reactive.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * Represents a Beer entity in the system.
+ * <p>
+ * This class is used to model the properties and behavior of a beer object,
+ * including its identification, name, style, UPC code, quantity on hand,
+ * price, and timestamps for creation and last modification.
+ * <p>
+ * Created by jt, Spring Framework Guru.
+ *
+ * @author architecture - rperezv
+ * @version 01/10/2024 - 15:49
+ * @since jdk 1.17
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+
+    /**
+     * The unique identifier for the beer.
+     */
+    @Id
+    private Integer id;
+
+    /**
+     * The name of the beer.
+     */
+    private String beerName;
+
+    /**
+     * The style of the beer (e.g., IPA, Stout).
+     */
+    private String beerStyle;
+
+    /**
+     * The Universal Product Code (UPC) for the beer.
+     */
+    private String upc;
+
+    /**
+     * The quantity of beer available on hand.
+     */
+    private Integer quantityOnHand;
+
+    /**
+     * The price of the beer.
+     */
+    private BigDecimal price;
+
+    /**
+     * The timestamp when the beer was created.
+     */
+    private LocalDateTime createdDate;
+
+    /**
+     * The timestamp when the beer was last modified.
+     */
+    private LocalDateTime lastModifiedDate;
+
+}
